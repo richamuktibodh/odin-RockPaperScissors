7842d96f67cd2a226ae204d5a225f52822627831
richamuktibodh <richa21086@iiitd.ac.in>
2024-01-27 23:19:24 +0530

    added ui

--- js/func.js
+++ js/func.js
@@ -23,40 +23,113 @@ function playRound(playerSelection, computerSelection)
     {
         return `U WON the round, ${playerSelection} beats ${computerSelection}`;
     }
+    return 'Choose wtv u want';
 }
+// reference to body
+const bodyReference = document.querySelector('body');
+// reference to div
+const divReference = document.querySelector('div');
+// make a new paragraph
+let pReference = document.createElement('p');
 
-function game()
+// making 3 new buttons
+const rockButton = document.createElement('button');
+rockButton.textContent = 'Rock';
+const paperButton = document.createElement('button');
+paperButton.textContent = 'Paper';
+const scissorsButton = document.createElement('button');
+scissorsButton.textContent = 'Scissors';
+
+function incrementScore(str)
 {
-    let player_score = 0, computer_score = 0;
-    for (let i = 0; i< 2; i++)
+    if (str.charAt(2) == 'L')
     {
-        // take playerSelection and computerSelection input
-        let playerSelection = prompt('Enter either rock, paper or scissors', 'rock');
-        let computerSelection = getComputerChoice();
-        let result = playRound(playerSelection, computerSelection);
-        if (result[2] == 'L')
-        {
-            computer_score++;
-        }
-        else
-        {
-            player_score++;
-        }
-        console.log(result, player_score, computer_score);
-    }
-    if (player_score> computer_score)
-    {
-        console.log(`You've won the game`);
-    }
-    else if (player_score< computer_score)
-    {
-        console.log(`You've lost the game`);
+        computer_score++;
     }
     else
     {
-        console.log(`It's a tie`);
+        player_score++;
     }
+    console.log(str, player_score, computer_score);
 }
 
-game();
+let player_choice = '', computer_choice = '';
+let player_score = 0, computer_score = 0;
+rockButton.addEventListener('click', function clickedRock()
+{
+    if (player_score < 5 && computer_score < 5)
+    {
+        player_choice = rockButton.textContent;
+        computer_choice = getComputerChoice();
+        pReference.textContent = playRound(player_choice, computer_choice);
+        incrementScore(pReference.textContent);
+        pReference.textContent+= ` player score: ${player_score} computer score: ${computer_score}`;
+    }
+    else
+    {
+        if (player_score> computer_score)
+        {
+            pReference.textContent = 'Congrats! u won the set of games!';
+        }
+        else
+        {
+            pReference.textContent = 'Hard Luck! the computer is smarter..';
+        }
+    }
+    divReference.appendChild(pReference);
+})
+
+paperButton.addEventListener('click', function clickedPaper()
+{
+    if (player_score < 5 && computer_score < 5)
+    {
+        player_choice = paperButton.textContent;
+        computer_choice = getComputerChoice();
+        pReference.textContent = playRound(player_choice, computer_choice);
+        incrementScore(pReference.textContent);
+        pReference.textContent+= ` player score: ${player_score} computer score: ${computer_score}`;
+    }
+    else
+    {
+        if (player_score> computer_score)
+        {
+            pReference.textContent = 'Congrats! u won the set of games!';
+        }
+        else
+        {
+            pReference.textContent = 'Hard Luck! the computer is smarter..';
+        }
+    }
+    divReference.appendChild(pReference);
+})
+
+scissorsButton.addEventListener('click', function clickedScissors()
+{
+    if (player_score < 5 && computer_score < 5)
+    {
+        player_choice = scissorsButton.textContent;
+        computer_choice = getComputerChoice();
+        pReference.textContent = playRound(player_choice, computer_choice);
+        incrementScore(pReference.textContent);
+        pReference.textContent+= ` player score: ${player_score} computer score: ${computer_score}`;
+    }
+    else
+    {
+        if (player_score> computer_score)
+        {
+            pReference.textContent = 'Congrats! u won the set of games!';
+        }
+        else
+        {
+            pReference.textContent = 'Hard Luck! the computer is smarter..';
+        }
+    }
+    divReference.appendChild(pReference);
+})
+
+bodyReference.appendChild(rockButton);
+bodyReference.appendChild(paperButton);
+bodyReference.appendChild(scissorsButton);
+
+// game();
 // getComputerChoice()
